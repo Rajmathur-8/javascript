d576470a61664961c7a0be980a043feec33d6c24
Raj Mathur <118900514+Rajmathur-8@users.noreply.github.com>
2023-12-01 12:30:39 +0530

    Function and Guessing game

---
+++ function/index.js
@@ -0,0 +1,45 @@
+
+
+// function happyBday(){
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday dear you");
+//     console.log("Happy Birthday to you!");
+// }
+
+// function happyBday(username,age){
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday to you!");
+//     console.log(`Happy Birthday dear ${username}!`);
+//     console.log("Happy Birthday to you!");
+//     console.log(`You are ${age} years old`);
+// }
+
+
+// happyBday("Raj",20);
+// happyBday("Spongebob",30);
+
+function add(a,b){
+    return a+b;
+}
+function subtract(x,y){
+    return x-y;
+}
+function multiply(x,y){
+    return x*y;
+}
+function divide(x,y){
+    return x/y;
+}
+
+
+console.log(add(6,3));
+console.log(subtract(6,3));
+console.log(multiply(6,3));
+console.log(divide(6,3));
+
+
+
+
+
+
