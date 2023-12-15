0f3651feca39b75b33a86910050c7cdc502e04ca
Raj Mathur <118900514+Rajmathur-8@users.noreply.github.com>
2023-12-15 23:45:04 +0530

    method

---
+++ ArrowFunction/index.js
@@ -0,0 +1,24 @@
+
+const hello = (name,age) => {console.log(`Hello ${name}`)
+                          console.log(`You are ${age} years Old`)  };
+
+hello("Harsh",20);
+
+
+setTimeout(calback, 3000);
+function hello(){
+    console.log("Hello")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
