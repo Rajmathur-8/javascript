102ee5d9cb1bd811f2501e52ab1a282ce54ea781
Raj Mathur <118900514+Rajmathur-8@users.noreply.github.com>
2023-12-17 20:53:07 +0530

    arrow function

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -5,16 +5,15 @@ const hello = (name,age) => {console.log(`Hello ${name}`)
 hello("Harsh",20);
 
 
-setTimeout(calback, 3000);
-function hello(){
-    console.log("Hello")
-}
+setTimeout( ()=> console.log("Hello"), 3000);
 
+const numbers = [1,2,3,4,5,6];
 
+const squares = numbers.map((element) => Math.pow(element,2));
+const cubes = numbers.map((element) => Math.pow(element,3));
 
-
-
-
+console.log(squares);
+console.log(cubes);
 
 
 
